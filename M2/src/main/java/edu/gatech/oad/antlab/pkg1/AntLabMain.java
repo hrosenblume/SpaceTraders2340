b2bc39a137812169940e3ae69aaea78af864e14e
Naman <namanshah66@gmail.com>
2014-09-04 16:34:04 -0400

    Edited AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("pvora7");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Naman Shah");
+                  toPrint += p5.toString("nshah310");
 		  
         System.out.println(toPrint);
         
