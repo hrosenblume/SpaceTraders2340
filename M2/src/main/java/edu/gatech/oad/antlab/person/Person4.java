fc4cf547c0a935c7319d6175d2f90750a02aff7e
Pranil Vora <pranilvora@Pranils-MacBook-Pro.local>
2014-09-04 00:13:00 -0400

    Completed Person4 required method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Pranil Vora
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String changedString = "";
+      for (int x = 0; x < input.length(); x++) {
+         changedString+= (char)(input.charAt(x) + 1);
+      }
+      return changedString;
     }
     
     /**
