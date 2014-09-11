90ee81d5b31e1cbb4136c348c7252a902cb36bdb
songstephen <stephen.song@gatech.edu>
2014-09-10 20:58:17 -0400

    Added preliminary UI structure for the title screen.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,56 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+        
+		initRootLayout();
+		showTitleScreen();
 	}
+	
+	private void initRootLayout() {
+		try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+	public void showTitleScreen() {
+		try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../view/TitleScreen.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
