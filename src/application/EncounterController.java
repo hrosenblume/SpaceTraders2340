90257b519c0364b5f82005eb01f981b4b557235b
songstephen <stephen.song@gatech.edu>
2014-10-23 10:41:44 -0400

    Added Javadoc and corrected formatting.

--- src/application/EncounterController.java
+++ src/application/EncounterController.java
@@ -16,83 +16,105 @@ import javafx.stage.Stage;
 
 import java.util.Random;
 
+/**
+ * Controls the logic behind random encounters.
+ * @author Stephen
+ */
 public class EncounterController implements Initializable {
-    @FXML
-    private Button okayButton;
-    @FXML
-    private Text planetToPlanet, randomEvent, encounterResult, fuelLeft;
-    static Planet lastPlanet, nextPlanet; 
+	@FXML
+	private Button okayButton;
+	@FXML
+	private Text planetToPlanet, randomEvent, encounterResult, fuelLeft;
+	static Planet lastPlanet, nextPlanet; 
 
-    public static void setPlanets(Planet lp, Planet np) {
-    	lastPlanet = lp;
-    	nextPlanet= np;
-    }
-    @FXML
-    private void buttonOK(ActionEvent event) throws IOException {
+	/**
+	 * Sets the planets the player is traveling between.
+	 * @param lp the planet the player is traveling from.
+	 * @param np the planet the player is traveling to.
+	 */
+	public static void setPlanets(Planet lp, Planet np) {
+		lastPlanet = lp;
+		nextPlanet= np;
+	}
+	@FXML
+	private void buttonOK(ActionEvent event) throws IOException {
 		Stage stage = (Stage) okayButton.getScene().getWindow();
-        stage.close();   	
-    }
-    private void updateTravelInfo() {
-    	planetToPlanet.setText(lastPlanet.getName() + " --> " + nextPlanet.getName());
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        updateTravelInfo();
-        updateFuel();
-        generateEvent();
-    }
-
-	private void updateFuel() {
-		fuelLeft.setText("Fuel left: " + Universe.player.getFuel());
-		
+		stage.close();   	
+	}
+	/**
+	 * Updates the travel label.
+	 */
+	private void updateTravelInfo() {
+		planetToPlanet.setText(lastPlanet.getName() + " --> " + nextPlanet.getName());
 	}
 
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		updateTravelInfo();
+		updateFuel();
+		generateEvent();
+	}
+
+	/**
+	 * Updates the fuel label.
+	 */
+	private void updateFuel() {
+		fuelLeft.setText("Fuel left: " + Universe.player.getFuel());
+
+	}
+
+	/**
+	 * Generates and processes a random event.
+	 */
 	private void generateEvent() {
 		String event = pickEvent();
 		String eventDescription = "";
 		String resultDescription = "";
 		Random numGen = new Random();
 		switch (event) {
-			case "Police" :
-				eventDescription = "The police have searched your ship and seized contraband!";
-				int moneyChange = numGen.nextInt(1000);
-				Universe.player.setMoney(Universe.player.getMoney() - moneyChange);
-				resultDescription = "Money: -" + moneyChange;
-				break;
-			case "Pirate" :
-				eventDescription = "A pirate attacked your ship and robbed you!";
-				int moneyChange2 = numGen.nextInt(2000);
-				Universe.player.setMoney(Universe.player.getMoney() - moneyChange2);
-				resultDescription = "Money: -" + moneyChange2;
-				break;
-			case "Lottery":
-				eventDescription = "You won the galactic lottery!";
-				int moneyChange3 = numGen.nextInt(5000);
-				Universe.player.setMoney(Universe.player.getMoney() + moneyChange3);
-				resultDescription = "Money: +" + moneyChange3;
-				break;
-			case "Asteroid" :
-				eventDescription = "Your ship hit an asteroid! Some machine parts were used in the repair.";
-				int machineChange = numGen.nextInt(500);
-				resultDescription = "Machines: -" + machineChange;
-				break;
-			case "DiscoverRobots" :
-				eventDescription = "You found high-value robot parts!";
-				int robotsChange = numGen.nextInt(100);
-				resultDescription = "Robots: +" + robotsChange;
-				break;
-			case "DiscoverMoney" :
-				eventDescription = "You found money randomly floating in space, which literally never happens!";
-				int moneyChange4 = numGen.nextInt(3000);
-				Universe.player.setMoney(Universe.player.getMoney() + moneyChange4);
-				resultDescription = "Money: +" + moneyChange4;
-				break;
+		case "Police" :
+			eventDescription = "The police have searched your ship and seized contraband!";
+			int moneyChange = numGen.nextInt(1000);
+			Universe.player.setMoney(Universe.player.getMoney() - moneyChange);
+			resultDescription = "Money: -" + moneyChange;
+			break;
+		case "Pirate" :
+			eventDescription = "A pirate attacked your ship and robbed you!";
+			int moneyChange2 = numGen.nextInt(2000);
+			Universe.player.setMoney(Universe.player.getMoney() - moneyChange2);
+			resultDescription = "Money: -" + moneyChange2;
+			break;
+		case "Lottery":
+			eventDescription = "You won the galactic lottery!";
+			int moneyChange3 = numGen.nextInt(5000);
+			Universe.player.setMoney(Universe.player.getMoney() + moneyChange3);
+			resultDescription = "Money: +" + moneyChange3;
+			break;
+		case "Asteroid" :
+			eventDescription = "Your ship hit an asteroid! Some machine parts were used in the repair.";
+			int machineChange = numGen.nextInt(500);
+			resultDescription = "Machines: -" + machineChange;
+			break;
+		case "DiscoverRobots" :
+			eventDescription = "You found high-value robot parts!";
+			int robotsChange = numGen.nextInt(100);
+			resultDescription = "Robots: +" + robotsChange;
+			break;
+		case "DiscoverMoney" :
+			eventDescription = "You found money randomly floating in space, which literally never happens!";
+			int moneyChange4 = numGen.nextInt(3000);
+			Universe.player.setMoney(Universe.player.getMoney() + moneyChange4);
+			resultDescription = "Money: +" + moneyChange4;
+			break;
 		}	
 		randomEvent.setText(eventDescription);
 		encounterResult.setText(resultDescription);
 	}
 
+	/**
+	 * Picks a random event to process.
+	 * @return the text representation of the event being done.
+	 */
 	private String pickEvent() {
 		Random rand = new Random();
 		String encounter = "";
@@ -102,12 +124,12 @@ public class EncounterController implements Initializable {
 		} else {
 			encounter = "RandomEvent";
 		}
-		
+
 		if (encounter.equals("PolicePirate")) {
 			String policeOrPirate = "";
 			probability = rand.nextInt(6);
 			if (probability < nextPlanet.policeIntensity()) {
-				 encounter = "Police";
+				encounter = "Police";
 			} else {
 				encounter = "Pirate";
 			}
@@ -123,5 +145,5 @@ public class EncounterController implements Initializable {
 		return encounter;
 	}  
 }
-    
+
 
