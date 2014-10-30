2d2392599b2dddeacecf90a21a0bb70d6ea6d3d3
Naman <namanshah66@gmail.com>
2014-10-30 02:36:43 -0400

    changed ship mintechlevels

--- src/application/Ship.java
+++ src/application/Ship.java
@@ -24,9 +24,9 @@ public class Ship {
 	
 	static HashMap<String, Ship> generateAllShips() {
 		HashMap<String, Ship> ships = new HashMap<>();
-		ships.put("flea", new Ship("flea", 10, 0, 0, 0, 1, 9999, 0, 1, 2000, 25, 1));
-		ships.put("gnat", new Ship("gnat", 15, 1, 0, 1, 1, 14, 2, 2, 10000, 100, 1));
-		ships.put("firefly", new Ship("firefly", 20, 1, 1, 1, 1, 17, 4, 3, 25000, 100, 1));
+		ships.put("flea", new Ship("flea", 10, 0, 0, 0, 1, 9999, 4, 1, 2000, 25, 1));
+		ships.put("gnat", new Ship("gnat", 15, 1, 0, 1, 1, 14, 5, 2, 10000, 100, 1));
+		ships.put("firefly", new Ship("firefly", 20, 1, 1, 1, 1, 17, 6, 3, 25000, 100, 1));
 		ships.put("mosquito", new Ship("mosquito", 15, 2, 1, 1, 1, 13, 6, 5, 30000, 100, 1));
 		ships.put("bulmblebee", new Ship("bumblebee", 25, 1, 2, 2, 2, 15, 7, 7, 60000, 100, 1));
 		return ships;
