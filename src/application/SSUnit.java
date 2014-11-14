dcc5c878e037ecbbdfa18b1d319f55ee1d4b5777
Stephen Song <songstephen@users.noreply.github.com>
2014-11-14 07:59:36 -0500

    Included missing brace in JUnit test.

--- src/application/SSUnit.java
+++ src/application/SSUnit.java
@@ -11,46 +11,49 @@
 //*/
 //public class SSUnit {
 //
-//   private Planet p;
-//   private Marketplace m;
+//    private Planet p;
+//    private Marketplace m;
 //
-//   @Test
-//       public void testFillMaps() {
+//    @Test
+//    public void testFillMaps() {
 //
-//           // init tests
-//           p = new Planet();
-//           p.planetTechInteger = 1;
-//           m = new Marketplace(p);
-//           p.marketplace = m;
+//        // init tests
+//        p = new Planet();
+//        p.planetTechInteger = 1;
+//        m = new Marketplace(p);
+//        p.marketplace = m;
 //
-//           // test early planet maps
-//           assertTrue(m.sellableResources.get("Water") != -1);
-//           assertTrue(m.buyableResources.get("Water") != -1);
-//           assertTrue(m.sellableResources.get("Furs") != -1);
-//           assertTrue(m.buyableResources.get("Furs") != -1);
-//           assertTrue(m.sellableResources.get("Food") != -1);
-//           assertTrue(m.buyableResources.get("Food") != -1);
-//           assertFalse(m.sellableResources.get("Ore") != -1);
-//           assertFalse(m.buyableResources.get("Ore") != -1);
-//           assertTrue(m.sellableResources.get("Games") != -1);
-//           assertFalse(m.buyableResources.get("Games") != -1);
-//           assertTrue(m.sellableResources.get("Firearms") != -1);
-//           assertFalse(m.buyableResources.get("Firearms") != -1);
-//           assertTrue(m.sellableResources.get("Medicine") != -1);
-//           assertFalse(m.buyableResources.get("Medicine") != -1);
-//           assertTrue(m.sellableResources.get("Narcotics") != -1);
-//           assertFalse(m.buyableResources.get("Narcotics") != -1);
-//           assertFalse(m.sellableResources.get("Machines") != -1);
-//           assertFalse(m.buyableResources.get("Machines") != -1);
-//           assertFalse(m.sellableResources.get("Robots") != -1);
-//           assertFalse(m.buyableResources.get("Robots") != -1);
-//           // test hi tech planet maps
-//           p.planetTechInteger = 6;
-//           m = new Marketplace(p);
-//           p.marketplace = m;
-//           for(java.util.Map.Entry<String, Resource> entry : Resource.resources.entrySet()) {
+//        // test early planet maps
+//        assertTrue(m.sellableResources.get("Water") != -1);
+//        assertTrue(m.buyableResources.get("Water") != -1);
+//        assertTrue(m.sellableResources.get("Furs") != -1);
+//        assertTrue(m.buyableResources.get("Furs") != -1);
+//        assertTrue(m.sellableResources.get("Food") != -1);
+//        assertTrue(m.buyableResources.get("Food") != -1);
+//        assertFalse(m.sellableResources.get("Ore") != -1);
+//        assertFalse(m.buyableResources.get("Ore") != -1);
+//        assertTrue(m.sellableResources.get("Games") != -1);
+//        assertFalse(m.buyableResources.get("Games") != -1);
+//        assertTrue(m.sellableResources.get("Firearms") != -1);
+//        assertFalse(m.buyableResources.get("Firearms") != -1);
+//        assertTrue(m.sellableResources.get("Medicine") != -1);
+//        assertFalse(m.buyableResources.get("Medicine") != -1);
+//        assertTrue(m.sellableResources.get("Narcotics") != -1);
+//        assertFalse(m.buyableResources.get("Narcotics") != -1);
+//        assertFalse(m.sellableResources.get("Machines") != -1);
+//        assertFalse(m.buyableResources.get("Machines") != -1);
+//        assertFalse(m.sellableResources.get("Robots") != -1);
+//        assertFalse(m.buyableResources.get("Robots") != -1);
+//        // test hi tech planet maps
+//        p.planetTechInteger = 6;
+//        m = new Marketplace(p);
+//        p.marketplace = m;
+//        for(java.util.Map.Entry<String, Resource> entry : Resource.resources.entrySet()) {
 //           String name = entry.getKey();
 //           assertTrue(m.sellableResources.get(name) != -1);
 //           assertTrue(m.buyableResources.get(name) != -1);
-//       }
-//}
+//       } // end loop
+//
+//    } // end testFillMaps()
+//
+//} // end class
